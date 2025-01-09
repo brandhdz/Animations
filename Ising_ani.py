8ad896f9294f4e03fc7b451ba41e09ede01940ae
Brandon Hernández <abrandhdz@gmail.com>
2025-01-09 17:56:11 -0600

    Ising_ani.py modify

--- Ising_ani.py
+++ Ising_ani.py
@@ -8,14 +8,14 @@ frames = 10
 grid = np.zeros((dim, dim))
 
 def animate(i):
-    grid = np.random.choice([0, 1], size = (dim, dim))
-    im = ax.imshow(grid, cmap = "bwr") 
+    grid[:] = np.random.choice([0, 1], size = (dim, dim))
+    im = ax.imshow(grid, cmap = "bwr")
 
-    return fig
+    return [im]
 
-fig, ax = plt.subplots() 
+fig, ax = plt.subplots()
 ax.get_xaxis().set_ticks([])
 ax.get_yaxis().set_ticks([])
 
-ani = FuncAnimation(fig, animate, frames = frames, interval = 200)
+ani = FuncAnimation(fig, animate, frames = frames, interval = 200, blit = True)
 ani.save("l_Ising.gif", writer = "pillow", fps = 5)
